6c75c6eec41b6d6ffe376e6b26caf3003ee49539
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-14 10:18:43 -0500

    commit tree e44c7ab16e5db4e40432bd173ab705c4c469baf0

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) {
+  return [array[0], array[1]];
+};
+
+const returnLastTwoDrivers = (array) => {
+  return [array[array.length - 2], array[array.length - 1]];
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (int) {
+    return int * 5;
+  };
+}
+
+const fareDoubler = (int) => {
+    return int * 2
+}
+
+const fareTripler = (int) => {
+    return int * 3
+}
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers)
+}
